c8bcff113c5aa9a3ebadbcd3941163912f1f029f
harjas27 <harjasbathla@gmail.com>
2017-05-29 10:01:47 +0530

    best fit line

---
+++ best_fit_line.py
@@ -0,0 +1,26 @@
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+xs = np.array([1,2,3,4,5], dtype=np.float64)
+ys = np.array([5,4,6,5,6], dtype=np.float64)
+
+def best_fit_slope_and_intercept(xs,ys):
+    m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
+         ((mean(xs)*mean(xs)) - mean(xs*xs)))
+    
+    b = mean(ys) - m*mean(xs)
+    
+    return m, b
+
+m, b = best_fit_slope_and_intercept(xs,ys)
+
+print(m,b)
+
+regression_line = [(m*x)+b for x in xs]
+
+plt.scatter(xs,ys,color='#003F72')
+plt.plot(xs, regression_line)
+plt.show()
